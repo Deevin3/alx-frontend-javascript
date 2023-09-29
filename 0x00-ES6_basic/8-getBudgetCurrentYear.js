f8a16549b05c71952b03fc7070eb50f4353ca0d9
Deevin3 <117818205+Deevin3@users.noreply.github.com>
2023-09-29 02:27:01 -0700

    first commit

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -2,12 +2,11 @@ function getCurrentYear() {
   const date = new Date();
   return date.getFullYear();
 }
+
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-  const year = getCurrentYear();
-  const budget = {
-    [`income-${year}`]: income,
-    [`gdp-${year}`]: gdp,
-    [`capita-${year}`]: capita,
+  return {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
   };
-    return budget;
 }
