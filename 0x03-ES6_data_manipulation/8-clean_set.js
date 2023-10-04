35f52e264141ce1fa9b4bcaaf62b4eba26228894
Deevin3 <117818205+Deevin3@users.noreply.github.com>
2023-10-04 07:15:56 -0700

    first commit

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,16 @@
-export default function cleanset(set, startstring) {
-  return array.from(set).filter(val => val.startsWith(startstring)).join('-')
+export default function cleanSet(set, startString) {
+  const parts = [];
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string') {
+    return '';
+  }
+  for (const value of set.values()) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      const valueSubStr = value.substring(startString.length);
+
+      if (valueSubStr && valueSubStr !== value) {
+        parts.push(valueSubStr);
+      }
+    }
+  }
+  return parts.join('-');
 }
